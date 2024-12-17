5cda6e834eb4fa52989ebf5671986da4da622a11
Link Li <nintendo.link@foxmail.com>
2024-12-17 16:59:41 +1100

    LL: Remove

--- LL.js
+++ LL.js
@@ -102,6 +102,20 @@ class LinkedList {
     this.length++;
     return true;
   }
+
+  remove(index) {
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    if (index < 0 || index >= this.length) return undefined;
+
+    let before = this.get(index - 1);
+    const temp = before.next;
+
+    before.next = temp.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
 }
 
 let myLinkedList = new LinkedList(1);
